b7e6a99152279e83a3026f6f6db67bd16f190128
lbuthman <lbuthman@gmail.com>
2017-09-06 05:38:51 -0500

    add first marker

--- js/app.js
+++ js/app.js
@@ -2,7 +2,15 @@ var map;
 function initMap() {
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7413549, lng: -73.9980244},
+    center: {lat: 32.934641, lng: -96.849289},
     zoom: 13
   });
+
+  var home = {lat: 32.928616, lng: -96.825974};
+
+  var marker = new google.maps.Marker({
+    position: home,
+    map: map,
+    title: "No place like home!"
+  })
 }
