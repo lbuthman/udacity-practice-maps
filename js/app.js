3276132372d22659e749a9cb63f05dd0e83f93f9
lbuthman <lbuthman@gmail.com>
2017-09-06 19:07:11 -0500

    add styles to map

--- js/app.js
+++ js/app.js
@@ -2,10 +2,61 @@ var map;
 var markers = [];
 
 function initMap() {
+
+  var styles = [
+    {
+      featureType: 'water',
+      stylers: [
+        { color: '#19a0d8' }
+      ]
+    },{
+      featureType: 'administrative',
+      elementType: 'labels.text.stroke',
+      stylers: [
+        { color: '#fff' },
+        { weight: 6 }
+      ]
+    },{
+      featureType: 'administrative',
+      elementType: 'labels.text.fill',
+      stylers: [
+        { color: '#e85113' }
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'geometry.stroke',
+      stylers: [
+        { color: '#efe9e4' },
+        { lightness: -40 }
+      ]
+    },{
+      featureType: 'transit.station',
+      stylers: [
+        { weight: 9 },
+        { hue: '#e85113' }
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'labels.icon',
+      stylers: [
+        { visibility: 'off' }
+      ]
+    },{
+      featureType: 'road.highway',
+      elementType: 'geometry.fill',
+      stylers: [
+        { color: '#efe9e4' },
+        { lightness: -25 }
+      ]
+    }
+  ];
+
   // Constructor creates a new map - only center and zoom are required.
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 32.934641, lng: -96.849289},
-    zoom: 13
+    zoom: 13,
+    styles: styles,
+    mapTypeControl: false
   });
 
   var locations = [
@@ -14,8 +65,23 @@ function initMap() {
     {title: "Carmine's Pizzeria", location: {lat: 32.939885, lng: -96.815144}}
   ];
 
-  var largeInfoWindonw = new google.maps.InfoWindow();
+  var largeInfoWindow = new google.maps.InfoWindow();
   var bounds = new google.maps.LatLngBounds();
+
+  var defaultIcon = makeMarkerIcon('0091ff');
+  var highlightedIcon = makeMarkerIcon('FFFF24');
+
+  function makeMarkerIcon(markerColor) {
+    var markerImage = new google.maps.MarkerImage(
+      'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+      '|40|_|%E2%80%A2',
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(10, 34),
+      new google.maps.Size(21, 34));
+
+    return markerImage
+  }
 
   for (var i=0; i<locations.length; i++) {
     var position = locations[i].location;
@@ -23,14 +89,25 @@ function initMap() {
     var marker = new google.maps.Marker({
       position: position,
       title: title,
+      icon: defaultIcon,
       animation: google.maps.Animation.DROP,
       id: i
     });
 
     markers.push(marker);
+
     bounds.extend(marker.position);
+
     marker.addListener('click', function(){
-      populateInfoWindow(this, largeInfoWindonw);
+      populateInfoWindow(this, largeInfoWindow);
+    });
+
+    marker.addListener('mouseover', function() {
+      this.setIcon(highlightedIcon);
+    });
+
+    marker.addListener('mouseout', function() {
+      this.setIcon(defaultIcon);
     });
 
     function populateInfoWindow(marker, infowindow) {
